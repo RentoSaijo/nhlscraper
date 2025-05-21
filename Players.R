dbc702c250f97f171a68fcb846c06ce4ecfe1a42
RentoSaijo <rentosaijo0527@gmail.com>
2025-05-21 18:36:19 +0900

    Add Players.R

---
+++ Players.R
@@ -0,0 +1,13 @@
+#' Get a player's game log for a season
+#' 
+#' @param player_id integer NHL player ID
+#' @param season string Season in 'YYYYYYYY' e.g. 20242025
+#' @param game_type integer 2=regular, 3=playoffs
+#' @return tibble with one row per game
+#' @export
+
+get_player_game_log <- function(player_id, season, game_type=2) {
+  path <- sprintf('player/%s/game-log/%s/%s', player_id, season, gametype)
+  out <- req_nhl(path)
+  return(tibble::as_tibble(out$data$gameLog))
+}
