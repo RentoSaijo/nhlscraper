979083efc7a7b0eae1c4daa7b910d6ab42361a19
RentoSaijo <rentosaijo0527@gmail.com>
2025-05-23 12:47:01 +0900

    Add several functions

--- R/Players.R
+++ R/Players.R
@@ -1,20 +1,7 @@
-#' Get a player's game log for a season
-#' 
-#' @param player_id integer NHL player ID
-#' @param season string Season in 'YYYYYYYY' e.g. 20242025
-#' @param game_type integer 2=regular, 3=playoffs
-#' @return tibble with one row per game
-#' @export
-
-get_player_game_log <- function(player_id, season, game_type=2) {
-  path <- sprintf('player/%s/game-log/%s/%s', player_id, season, game_type)
-  out <- req_nhl(path)
-  return(tibble::as_tibble(out$gameLog))
-}
-
 #' Get all players' ids, first and last names, and multiplicities from
 #' specified seasons
 #' 
+#' @importFrom magrittr %>%
 #' @param start_year integer Year to start search
 #' @param end_year integer Year to end search
 #' @return dataframe with one row per player
@@ -26,14 +13,11 @@ get_players <- function(start_year=1917, end_year=2025) {
     start_year:(end_year-1),
     sprintf("%04d", (start_year:(end_year-1)) + 1)
   )
-  
   # placeholder for each season (regular & playoffs)
   all_players <- list()
-  
   # loop through seasons (regular & playoffs)
   for (season in seasons) {
     for (rp in c(2, 3)) {
-      
       # endpoint path
       path <- paste0(
         'skater-stats-leaders/',
@@ -41,27 +25,22 @@ get_players <- function(start_year=1917, end_year=2025) {
         '/',
         rp
       )
-      
-      # hit the API
-      pts <- req_nhl(path, list(categories='points', limit=-1))$points
-      
+      # hit API
+      toi <- req_nhl(path, list(categories='toi', limit=-1))$toi
       # skip if empty
-      if (is.null(pts) || nrow(pts) == 0) {
+      if (is.null(toi) || nrow(toi) == 0) {
         next
       }
-      
       # tibble for this batch
       df <- tibble::tibble(
-        PlayerID=pts$id,
-        FirstName=pts$`firstName.default`,
-        LastName=pts$`lastName.default`
+        PlayerID=toi$id,
+        FirstName=toi$`firstName.default`,
+        LastName=toi$`lastName.default`
       )
-      
       # append to all_players
       all_players[[length(all_players)+1]] <- df
     }
   }
-  
   # combine, dedupe, and compute multiplicity
   dplyr::bind_rows(all_players) %>%
     dplyr::distinct(PlayerID, FirstName, LastName) %>%
@@ -70,4 +49,48 @@ get_players <- function(start_year=1917, end_year=2025) {
     dplyr::mutate(Multiplicity = dplyr::row_number()) %>%
     dplyr::ungroup() %>% 
     dplyr::arrange(PlayerID)
+}
+
+#' Get a player's game log for a season
+#' 
+#' @param player_id integer NHL player ID
+#' @param season string Season in 'YYYYYYYY' e.g. 20242025
+#' @param game_type integer 2=regular, 3=playoffs
+#' @return tibble with one row per game
+#' @export
+
+get_player_game_log <- function(player_id, season=20242025, game_type=2) {
+  path <- sprintf('player/%s/game-log/%s/%s', player_id, season, game_type)
+  out <- req_nhl(path)
+  return(tibble::as_tibble(out$gameLog))
+}
+
+#' Get a player's information
+#' 
+#' @param player_id integer NHL player ID
+#' @return list with 36 items
+#' @export
+
+get_player_information <- function(player_id) {
+  path <- sprintf('player/%s/landing', player_id)
+  out <- req_nhl(path)
+  return(out)
+}
+
+#' Get skater stats leaders for a season
+#' 
+#' @param season string Season in 'YYYYYYYY' e.g. 20242025
+#' @param game_type integer 2=regular, 3=playoffs
+#' @return tibble with one row per skater
+#' @export
+
+get_skater_leaders <- function(
+    season=20242025,
+    game_type=2,
+    category='points',
+    limit=-1
+  ) {
+  path <- sprintf('skater-stats-leaders/%s/%s', season, game_type)
+  out <- req_nhl(path, list(categories=category, limit=limit))
+  return(tibble::as_tibble(out[[category]]))
 }
