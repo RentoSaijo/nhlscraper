05648082edc16ec4c2f88949c71f728e6e085731
RentoSaijo <rentosaijo0527@gmail.com>
2025-05-23 14:05:32 +0900

    Fix get_skaters(), add get_goalies()

--- R/Players.R
+++ R/Players.R
@@ -1,4 +1,4 @@
-#' Get all players' ids, first and last names, and multiplicities from
+#' Get all skaters' ids, first and last names, and multiplicities from
 #' specified seasons (who have at least scored 1 point)
 #' 
 #' @importFrom magrittr %>%
@@ -7,14 +7,14 @@
 #' @return dataframe with one row per player
 #' @export
 
-get_players <- function(start_year=1917, end_year=2025) {
+get_skaters <- function(start_year=1917, end_year=2025) {
   # build seasons
   seasons <- paste0(
     start_year:(end_year-1),
-    sprintf("%04d", (start_year:(end_year-1)) + 1)
+    sprintf("%04d", (start_year:(end_year-1))+1)
   )
   # placeholder for each season (regular & playoffs)
-  all_players <- list()
+  all_skaters <- list()
   # loop through seasons (regular & playoffs)
   for (season in seasons) {
     for (rp in c(2, 3)) {
@@ -26,9 +26,9 @@ get_players <- function(start_year=1917, end_year=2025) {
         rp
       )
       # hit API
-      pts <- req_nhl(path, list(categories='pts', limit=-1))$pts
+      pts <- req_nhl(path, list(categories='points', limit=-1))$points
       # skip if empty
-      if (is.null(pts) || nrow(pts) == 0) {
+      if (is.null(pts) || nrow(pts)==0) {
         next
       }
       # tibble for this batch
@@ -37,12 +37,57 @@ get_players <- function(start_year=1917, end_year=2025) {
         FirstName=pts$`firstName.default`,
         LastName=pts$`lastName.default`
       )
-      # append to all_players
-      all_players[[length(all_players)+1]] <- df
+      # append to all_skaters
+      all_skaters[[length(all_skaters)+1]] <- df
     }
   }
   # combine, dedupe, and compute multiplicity
-  dplyr::bind_rows(all_players) %>%
+  dplyr::bind_rows(all_skaters) %>%
+    dplyr::distinct(PlayerID, FirstName, LastName) %>%
+    dplyr::group_by(FirstName, LastName) %>%
+    dplyr::arrange(PlayerID, .by_group=T) %>%
+    dplyr::mutate(Multiplicity = dplyr::row_number()) %>%
+    dplyr::ungroup() %>% 
+    dplyr::arrange(PlayerID)
+}
+
+#' Get all goalies' ids, first and last names, and multiplicities from
+#' specified seasons (who have at least earned 1 win)
+#' 
+#' @importFrom magrittr %>%
+#' @param start_year integer Year to start search
+#' @param end_year integer Year to end search
+#' @return dataframe with one row per player
+#' @export
+
+get_goalies <- function(start_year=1917, end_year=2025) {
+  # same logic as get_skaters()
+  seasons <- paste0(
+    start_year:(end_year-1),
+    sprintf("%04d", (start_year:(end_year-1)) + 1)
+  )
+  all_goalies <- list()
+  for (season in seasons) {
+    for (rp in c(2, 3)) {
+      path <- paste0(
+        'goalie-stats-leaders/',
+        season,
+        '/',
+        rp
+      )
+      wins <- req_nhl(path, list(categories='wins', limit=-1))$wins
+      if (is.null(wins) || nrow(wins) == 0) {
+        next
+      }
+      df <- tibble::tibble(
+        PlayerID=wins$id,
+        FirstName=wins$`firstName.default`,
+        LastName=wins$`lastName.default`
+      )
+      all_goalies[[length(all_goalies)+1]] <- df
+    }
+  }
+  dplyr::bind_rows(all_goalies) %>%
     dplyr::distinct(PlayerID, FirstName, LastName) %>%
     dplyr::group_by(FirstName, LastName) %>%
     dplyr::arrange(PlayerID, .by_group=T) %>%
@@ -81,6 +126,9 @@ get_player_information <- function(player_id) {
 #' 
 #' @param season string Season in 'YYYYYYYY' e.g. 20242025
 #' @param game_type integer 2=regular, 3=playoffs
+#' @param category string e.g. assists, goals, goalsSh, goalsPp, points,
+#' penaltyMins, toi, plusMinus, faceoffLeaders
+#' (note that some are only available in recent years)
 #' @return tibble with one row per skater
 #' @export
 
@@ -91,6 +139,26 @@ get_skater_leaders <- function(
     limit=-1
   ) {
   path <- sprintf('skater-stats-leaders/%s/%s', season, game_type)
+  out <- req_nhl(path, list(categories=category, limit=limit))
+  return(tibble::as_tibble(out[[category]]))
+}
+
+#' Get goalie stats leaders for a season
+#' 
+#' @param season string Season in 'YYYYYYYY' e.g. 20242025
+#' @param game_type integer 2=regular, 3=playoffs
+#' @param category string e.g. wins, shutouts, savePctg, goalsAgainstAverage
+#' (note that some are only available in recent years)
+#' @return tibble with one row per skater
+#' @export
+
+get_goalie_leaders <- function(
+    season=20242025,
+    game_type=2,
+    category='wins',
+    limit=-1
+  ) {
+  path <- sprintf('goalie-stats-leaders/%s/%s', season, game_type)
   out <- req_nhl(path, list(categories=category, limit=limit))
   return(tibble::as_tibble(out[[category]]))
 }
