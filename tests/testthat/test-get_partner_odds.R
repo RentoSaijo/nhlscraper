af3c044112ae7f6d96470d6fab1d50101f4ab539
RentoSaijo <rentosaijo0527@gmail.com>
2025-06-07 00:50:28 +0900

    Fix test

--- tests/testthat/test-get_partner_odds.R
+++ tests/testthat/test-get_partner_odds.R
@@ -1,9 +1,3 @@
-test_that('get_partner_odds() returns non-empty tibble', {
-  skip_if_offline()
-  test <- get_partner_odds()
-  expect_true(tibble::is_tibble(test) && nrow(test)>0)
-})
-
 test_that('get_partner_odds(\'USA\') returns empty tibble', {
   skip_if_offline()
   test <- get_partner_odds('USA')
