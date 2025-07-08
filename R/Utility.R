a2576adce08f4bee614b607d675f572388e7503a
RentoSaijo <rentosaijo0527@gmail.com>
2025-07-08 17:49:48 +0900

    Add utility for ESPN API

--- R/Utility.R
+++ R/Utility.R
@@ -5,7 +5,6 @@
 #' @param stats_rest boolean isStatsRest where TRUE=NHL's REST API and FALSE=
 #'                   NHL's standard API
 #' @return parsed JSON
-#' 
 #' @keywords internal
 
 nhl_api <- function(path, query=list(), stats_rest=FALSE) {
@@ -19,6 +18,27 @@ nhl_api <- function(path, query=list(), stats_rest=FALSE) {
   }
   else {
     base <- 'https://api-web.nhle.com/v1/'
+  }
+  url <- paste0(base, path)
+  resp <- httr::GET(url, query=query)
+  json <- httr::content(resp, as='text', encoding='UTF-8')
+  return(jsonlite::fromJSON(json, simplifyVector=TRUE, flatten=TRUE))
+}
+
+#' Internal: call ESPN API
+#' 
+#' @param path String API path
+#' @param query list Query parameters
+#' @param type integer where 1=site.api and 2=sports.core
+#' @return parsed JSON
+#' @keywords internal
+
+espn_api <- function(path, query=list(), type=1) {
+  if (type==1) {
+    base <- 'https://site.api.espn.com/apis/site/v2/sports/hockey/nhl/'
+  }
+  else if (type==2){
+    base <- 'https://sports.core.api.espn.com/v2/sports/hockey/leagues/nhl'
   }
   url <- paste0(base, path)
   resp <- httr::GET(url, query=query)
