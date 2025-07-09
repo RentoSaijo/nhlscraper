54f8e047b16d8b5f102d37cda361488440dd1e16
RentoSaijo <rentosaijo0527@gmail.com>
2025-07-09 19:50:54 +0900

    Revert template

--- R/Test.R
+++ R/Test.R
@@ -3,8 +3,7 @@
 library(httr)
 library(jsonlite)
 
-url <- "https://records.nhl.com/site/api/award-details"
+url <- "https://records.nhl.com/site/api/draft-master"
 resp <- httr::GET(url)
 json <- httr::content(resp, as='text', encoding='UTF-8')
 out <- jsonlite::fromJSON(json, simplifyVector=TRUE, flatten=TRUE)
-
