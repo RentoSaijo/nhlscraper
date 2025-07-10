11c4993ba2014408c1ffeb4999ea9be36b167800
RentoSaijo <rentosaijo0527@gmail.com>
2025-07-10 19:57:35 +0900

    Add get_futures()

--- R/Betting.R
+++ R/Betting.R
@@ -14,18 +14,19 @@ get_partner_odds <- function(country='US') {
   return(tibble::as_tibble(out$games))
 }
 
-#' Get ESPN futures as of now
+#' Get ESPN futures by season
 #' 
-#' @param country string 2-letter country code e.g. 'US'
-#' @return tibble with one row per game
+#' @param season integer Season in YYYYYYY
+#' @return tibble with one row per future
 #' @examples
-#' partner_odds_now_CA <- get_partner_odds(country='CA')
+#' futures_20242025 <- get_futures(20242025)
 #' @export
 
-get_partner_odds <- function(country='US') {
-  out <- nhl_api(
-    path=sprintf('partner-game/%s/now', country),
-    type=1
+get_futures <- function(season=get_season_now$seasonId) {
+  out <- espn_api(
+    path=sprintf('seasons/%s/futures', nhl_season_to_espn_season(season)),
+    query=list(lang='en', region='us', limit=1000),
+    type=2
   )
   return(tibble::as_tibble(out$games))
 }
