e1e18f85c04b41485de0d943a01f6ddb6cdc6dbe
RentoSaijo <rentosaijo0527@gmail.com>
2025-07-17 18:43:35 +0900

    Update ESPN team functions' documentation

--- R/ESPNTeam.R
+++ R/ESPNTeam.R
@@ -1,5 +1,7 @@
 #' Get ESPN teams by season
 #' 
+#' `get_espn_teams()` retrieves ESPN hyperlinks for each team for a given `season`; the hyperlinks are formatted in `base/seasons/{ESPN Season ID}/coaches/{ESPN Team ID}?query`. Access `get_seasons()` for `season` reference. May soon be reworked to only return the ESPN Team IDs.
+#' 
 #' @param season integer Season in YYYY
 #' @return tibble with one row per team
 #' @examples
@@ -17,14 +19,16 @@ get_espn_teams <- function(season=get_season_now()$seasonId%%10000) {
 
 #' Get team by season and ESPN Team ID
 #' 
-#' @param season integer Season in YYYY
+#' `get_espn_team()` retrieves information on a `team` for a given `season`, including but not limited to its name and logo URL. Access `get_espn_teams()` for `team` and `get_seasons()` for `season` references.
+#' 
 #' @param team integer ESPN Team ID
+#' @param season integer Season in YYYY
 #' @return list with various items
 #' @examples
-#' ESPN_BOS_20242025 <- get_espn_team(season=2025, team=1)
+#' ESPN_BOS_20242025 <- get_espn_team(team=1, season=2025)
 #' @export
 
-get_espn_team <- function(season=get_season_now()$seasonId%%10000, team=1) {
+get_espn_team <- function(team=1, season=get_season_now()$seasonId%%10000) {
   out <- espn_api(
     path=sprintf('seasons/%s/teams/%s', season, team),
     query=list(lang='en', region='us', limit=1000),
