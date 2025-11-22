acbe5077766e92474cd8960e1057033e713b1b0b
RentoSaijo <rentosaijo0527@gmail.com>
2025-11-22 14:47:47 -0500

    Move functions

--- R/League.R
+++ R/League.R
@@ -132,3 +132,20 @@ ns_schedule <- function(date = Sys.Date()) {
     }
   )
 }
+
+#' Get the attendance for all the seasons
+#' 
+#' `ns_attendance()` retrieves information on each season, including but not 
+#' limited to their ID and regular and playoff attendance.
+#' 
+#' @return data.frame with one row per season
+#' @examples
+#' all_attendance <- ns_attendance()
+#' @export
+
+ns_attendance <- function() {
+  nhl_api(
+    path = 'attendance',
+    type = 'r'
+  )$data
+}
