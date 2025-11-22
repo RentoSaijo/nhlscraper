a7860ef40660686f869fb3ed6f49ccebcdcfe363
RentoSaijo <rentosaijo0527@gmail.com>
2025-11-21 22:26:51 -0500

    Add new game functions

--- R/Game.R
+++ R/Game.R
@@ -1,199 +1,278 @@
-#' Get all games
+#' Get all the games
 #' 
-#' `get_games()` retrieves information on each game, including but not limited to their ID, season, type, start date and time, and home and visiting teams' IDs and scores.
+#' `ns_games()` retrieves information on each game, including but not limited 
+#' to their ID, season, type, start date and time, and home and visiting teams' 
+#' IDs and scores.
 #' 
-#' @return tibble with one row per game
+#' @return data.frame with one row per game
 #' @examples
 #' # This may take >5s, so skip.
 #' \donttest{
-#'   all_games <- get_games()
+#'   all_games <- ns_games()
 #' }
 #' @export
 
-get_games <- function() {
-  out <- nhl_api(
-    path='game',
-    type=2
-  )
-  return(tibble::as_tibble(out$data))
+ns_games <- function() {
+  nhl_api(
+    path = 'en/game',
+    type = 's'
+  )$data
 }
 
-#' Get score(s) by date
+#' Get the score(s) of the game(s) for a date
 #' 
-#' `get_scores()` retrieves information on each game for a given `date`, including but not limited to their ID; type; venue; start time; period and intermission clocks; and home and away teams' IDs, names, and scores. Access `get_seasons()` for `date` reference.
+#' `ns_scores()` retrieves information on each game for a given `date`, 
+#' including but not limited to their ID; type; venue; start time; period and 
+#' intermission clocks; and home and away teams' IDs, names, and scores. Access 
+#' `get_seasons()` for `date` reference.
 #' 
-#' @param date string in 'YYYY-MM-DD'
-#' @return tibble with one row per game
+#' @param date character in 'YYYY-MM-DD' (e.g., '2025-01-01')
+#' @return data.frame with one row per game
 #' @examples
-#' scores_2025_01_02 <- get_scores(date='2025-01-02')
+#' scores_Halloween_2025 <- ns_scores(date = '2025-10-31')
 #' @export
 
-get_scores <- function(date='2025-01-01') {
-  if (!grepl('^\\d{4}-\\d{2}-\\d{2}$', date)) {
-    stop('`date` must be in \'YYYY-MM-DD\' format', call.=FALSE)
-  }
-  out <- nhl_api(
-    path=sprintf('score/%s', date),
-    type=1
+ns_scores <- function(date = 'now') {
+  tryCatch(
+    expr = {
+      nhl_api(
+        path = sprintf('v1/score/%s', date),
+        type = 'w'
+      )$games
+    },
+    error = function(e) {
+      message("Invalid argument(s); refer to help file.")
+      data.frame()
+    }
   )
-  return(tibble::as_tibble(out$games))
 }
 
-#' Get scoreboard(s) by date
+#' Get the rosters of a game
 #' 
-#' `get_scoreboards()` retrieves information on each game for a given `date`, including but not limited to their ID; type; venue; start time; tickets link; and home and away teams' IDs, names, and scores. Access `get_seasons()` for `date` reference. Unable to conclude any major difference versus `get_scores()`; may soon be deprecated.
+#' `ns_game_rosters()` retrieves ...
 #' 
-#' @param date string in 'YYYY-MM-DD'
-#' @return tibble with one row per game
+#' @param game integer ID (e.g., 2025020275)
+#' @return data.frame with one row per player
 #' @examples
-#' scoreboards_2025_01_02 <- get_scoreboards(date='2025-01-02')
+#' rosters_SCF_game_7_20232024 <- ns_game_rosters(game = 2023030417)
 #' @export
 
-get_scoreboards <- function(date='2025-01-01') {
-  if (!grepl('^\\d{4}-\\d{2}-\\d{2}$', date)) {
-    stop('`date` must be in \'YYYY-MM-DD\' format', call.=FALSE)
-  }
-  out <- nhl_api(
-    path=sprintf('scoreboard/%s', date),
-    type=1
+ns_game_rosters <- function(game = 2025020275) {
+  tryCatch(
+    expr = {
+      nhl_api(
+        path = sprintf('v1/gamecenter/%s/play-by-play', game),
+        type = 'w'
+      )$rosterSpots
+    },
+    error = function(e) {
+      message("Invalid argument(s); refer to help file.")
+      data.frame()
+    }
   )
-  if (is.null(out$gamesByDate)) {
-    return(tibble::tibble())
-  }
-  sub <- out$gamesByDate[out$gamesByDate$date==date, , drop=FALSE]
-  if (nrow(sub)==0) {
-    return(tibble::tibble())
-  }
-  tibble::as_tibble(sub$games[[1]])
 }
 
-#' Get boxscore by game, team, and player-type
+#' Get the boxscore of a game for a team and position
 #' 
-#' `get_game_boxscore()` retrieves information on each player for a given set of `game`, `team`, and `player_type`, including but not limited to their ID, name, sweater number, goals, assists, +/-, hits, blocks, shots-on-goal, giveaways, takeaways, time on ice, and number of shifts. Access `get_games()` for `game` reference.
+#' `ns_boxscore()` retrieves information on each player for a given set 
+#' of `game`, `team`, and `player_type`, including but not limited to their ID, 
+#' name, sweater number, goals, assists, +/-, hits, blocks, shots-on-goal, 
+#' giveaways, takeaways, time on ice, and number of shifts. Access 
+#' `get_games()` for `game` reference.
 #' 
-#' @param game integer Game ID
-#' @param team string of 'home' or 'away'
-#' @param player_type string of 'forwards', 'defense', or 'goalies'
-#' @return tibble with one row per player
+#' @param game integer ID (e.g., 2025020275)
+#' @param team character of 'h'/'home' or 'a'/'away'
+#' @param position character of 'f'/'forwards', 'd'/'defensemen', or 
+#' 'g'/goalies'
+#' @return data.frame with one row per player
 #' @examples
-#' boxscore_2024030411_FLA_defensemen <- get_game_boxscore(
-#'   game=2024030411,
-#'   team='away',
-#'   player_type='defense'
+#' boxscore_SCF_game_7_20232024_FLA_defensemen <- ns_boxscore(
+#'   game     = 2023030417,
+#'   team     = 'A',
+#'   position = 'D'
 #' )
 #' @export
 
-get_game_boxscore <- function(
-    game=2024020602,
-    team='home',
-    player_type='forwards'
+ns_boxscore <- function(
+    game     = 2025020275,
+    team     = 'home',
+    position = 'forwards'
 ) {
-  out <- nhl_api(
-    path=sprintf('gamecenter/%s/boxscore', game),
-    type=1
-  )
-  return(tibble::as_tibble(
-    out$playerByGameStats[[paste0(team, 'Team')]][[player_type]])
+  tryCatch(
+    expr = {
+      team <- switch(
+        substring(tolower(team), 1, 1),
+        h = 'home',
+        a = 'away'
+      )
+      position <- switch(
+        substring(tolower(position), 1, 1),
+        f = 'forwards',
+        d = 'defense',
+        g = 'goalies'
+      )
+      boxscore <- nhl_api(
+        path = sprintf('v1/gamecenter/%s/boxscore', game),
+        type = 'w'
+      )$playerByGameStats
+      boxscore[[paste0(team, 'Team')]][[position]]
+    },
+    error = function(e) {
+      message("Invalid argument(s); refer to help file.")
+      data.frame()
+    }
   )
 }
 
-#' Get GameCenter (GC) play-by-play by game
+#' Get the GameCenter (GC) play-by-play of a game
 #' 
-#' `get_gc_play_by_play()` retrieves GC-provided information on each play for a given `game`, including but not limited to their ID; type; time of occurrence; winning, losing, blocking, shooting, hitting, hit, scoring, assisting, committed-by, drawn-by, and/or served-by player IDs; and X and Y coordinates. Access `get_games()` for `game` reference.
+#' `ns_gc_play_by_play()` retrieves GC-provided information on each play for a 
+#' given `game`, including but not limited to their ID; type; time of 
+#' occurrence; winning, losing, blocking, shooting, hitting, hit, scoring, 
+#' assisting, committed-by, drawn-by, and/or served-by player IDs; and X and Y 
+#' coordinates. Access `get_games()` for `game` reference.
 #' 
-#' @param game integer Game ID
-#' @return tibble with one row per play
+#' @param game integer ID (e.g., 2025020275)
+#' @return data.frame with one row per event/play
 #' @examples
-#' gc_pbp_2024030411 <- get_gc_play_by_play(game=2024030411)
+#' gc_pbp_SCF_game_7_20232024 <- ns_gc_play_by_play(game = 2023030417)
 #' @export
 
-get_gc_play_by_play <- function(game=2024020602) {
-  out <- nhl_api(
-    path=sprintf('gamecenter/%s/play-by-play', game),
-    type=1
+ns_gc_play_by_play <- function(game = 2025020275) {
+  tryCatch(
+    expr = {
+      nhl_api(
+        path = sprintf('v1/gamecenter/%s/play-by-play', game),
+        type = 'w'
+      )$plays
+    },
+    error = function(e) {
+      message("Invalid argument(s); refer to help file.")
+      data.frame()
+    }
   )
-  return(tibble::as_tibble(out$plays))
 }
 
-#' Get World Showcase (WSC) play-by-play by game
+#' @rdname ns_gc_play_by_play
+#' @export
+ns_gc_pbp <- function(game = 2025020275) {
+  ns_gc_pbp(game)
+}
+
+#' Get the World Showcase (WSC) play-by-play of a game
 #' 
-#' `get_wsc_play_by_play()` retrieves WSC-provided information on each play for a given `game`, including but not limited to their ID; time and strength state of occurrence; winning, losing, blocking, shooting, hitting, hit, scoring, assisting, committed-by, drawn-by, and/or served-by player IDs; and X and Y coordinates. Access `get_games()` for `game` reference.
+#' `ns_wsc_play_by_play()` retrieves WSC-provided information on each play for a given `game`, including but not limited to their ID; time and strength state of occurrence; winning, losing, blocking, shooting, hitting, hit, scoring, assisting, committed-by, drawn-by, and/or served-by player IDs; and X and Y coordinates. Access `get_games()` for `game` reference.
 #'
-#' @param game integer Game ID
-#' @return tibble with one row per play
+#' @param game integer ID (e.g., 2025020275)
+#' @return data.frame with one row per event/play
 #' @examples
-#' wsc_pbp_2024030411 <- get_wsc_play_by_play(game=2024030411)
+#' wsc_pbp_SCF_game_7_20232024 <- ns_wsc_play_by_play(game = 2023030417)
 #' @export
 
-get_wsc_play_by_play <- function(game=2024020602) {
-  out <- nhl_api(
-    path=sprintf('wsc/play-by-play/%s', game),
-    type=1
+ns_wsc_play_by_play <- function(game = 2025020275) {
+  tryCatch(
+    expr = {
+      nhl_api(
+        path = sprintf('v1/wsc/play-by-play/%s', game),
+        type = 'w'
+      )
+    },
+    error = function(e) {
+      message("Invalid argument(s); refer to help file.")
+      data.frame()
+    }
   )
-  out <- tibble::as_tibble(out)
-  if (ncol(out)==4) {
-    return(tibble::tibble())
-  }
-  return(out)
 }
 
-#' Get shift charts
+#' @rdname ns_wsc_play_by_play
+#' @export
+ns_wsc_pbp <- function(game = 2025020275) {
+  ns_wsc_pbp(game)
+}
+
+#' Get the shifts of a game
 #' 
-#' `get_shift_charts()` retrieves information on each shift for a given `game`, including but not limited to their period, start and end times, and player's ID and name. Access `get_games()` for `game` reference.
+#' `ns_shifts()` retrieves information on each shift for a given `game`, 
+#' including but not limited to their period, start and end times, and player's 
+#' ID and name. Access `get_games()` for `game` reference.
 #' 
-#' @param game integer Game ID
-#' @return tibble with one row per shift
+#' @param game integer ID (e.g., 2025020275)
+#' @return data.frame with one row per shift
 #' @examples
-#' shift_charts_2024030411 <- get_shift_charts(game=2024030411)
+#' shifts_SCF_game_7_20232024 <- ns_shifts(game = 2023030417)
 #' @export
 
-get_shift_charts <- function(game=2024020602) {
-  out <- nhl_api(
-    path='shiftcharts',
-    query=list(cayenneExp=sprintf('gameId=%s', game)),
-    type=2
+ns_shifts <- function(game = 2025020275) {
+  tryCatch(
+    expr = {
+      nhl_api(
+        path  = 'en/shiftcharts',
+        query = list(cayenneExp = sprintf('gameId = %s', game)),
+        type  = 's'
+      )$data
+    },
+    error = function(e) {
+      message("Invalid argument(s); refer to help file.")
+      data.frame()
+    }
   )
-  return(tibble::as_tibble(out$data))
 }
 
-#' Get GameCenter (GC) game-landing by game
+#' Get the GameCenter (GC) summary of a game
 #' 
-#' `get_game_landing()` retrieves GC-provided information on a `game`, including but not limited to its type, venue, start time, clock, home and away teams, and TV broadcast(s). Access `get_games()` for `game` reference.
+#' `ns_gc_summary()` retrieves GC-provided information on a `game`, 
+#' including but not limited to its type, venue, start time, clock, home and 
+#' away teams, and TV broadcast(s). Access `get_games()` for `game` reference.
 #' 
-#' @param game integer Game ID
+#' @param game integer ID (e.g., 2025020275)
 #' @return list of various items
 #' @examples
-#' game_landing_2024030411 <- get_game_landing(game=2024030411)
+#' gc_summary_SCF_game_7_20232024 <- ns_gc_summary(game = 2023030417)
 #' @export
 
-get_game_landing <- function(game=2024020602) {
-  out <- nhl_api(
-    path=sprintf('gamecenter/%s/landing', game),
-    type=1
+ns_gc_summary <- function(game = 2025020275) {
+  tryCatch(
+    expr = {
+      landing    <- nhl_api(
+        path = sprintf('v1/gamecenter/%s/landing', game),
+        type = 'w'
+      )
+      right_rail <- nhl_api(
+        path = sprintf('v1/gamecenter/%s/right-rail', game),
+        type = 'w'
+      )
+      c(landing, right_rail)
+    },
+    error = function(e) {
+      message("Invalid argument(s); refer to help file.")
+      data.frame()
+    }
   )
-  if (length(out)==4) {
-    return(list())
-  }
-  return(out)
 }
 
-#' Get World Showcase (WSC) game-story by game
+#' Get the World Showcase (WSC) summary of a game
 #' 
-#' `get_game_story()` retrieves WSC-provided information on a `game`, including but not limited to its type, venue, start time, clock, home and away teams, and TV broadcast(s). Access `get_games()` for `game` reference.
+#' `ns_wsc_summary()` retrieves WSC-provided information on a `game`, including 
+#' but not limited to its type, venue, start time, clock, home and away teams, 
+#' and TV broadcast(s). Access `get_games()` for `game` reference.
 #' 
-#' @param game integer Game ID
+#' @param game integer ID (e.g., 2025020275)
 #' @return list of various items
 #' @examples
-#' game_story_2024030411 <- get_game_story(game=2024030411)
+#' wsc_summary_SCF_game_7_20232024 <- ns_wsc_summary(game = 2023030417)
 #' @export
 
-get_game_story <- function(game=2024020602) {
-  out <- nhl_api(
-    path=sprintf('wsc/game-story/%s', game),
-    type=1
+ns_wsc_summary <- function(game = 2025020275) {
+  tryCatch(
+    expr = {
+      nhl_api(
+        path = sprintf('v1/wsc/game-story/%s', game),
+        type = 'w'
+      )
+    },
+    error = function(e) {
+      message("Invalid argument(s); refer to help file.")
+      data.frame()
+    }
   )
-  if (length(out)==4) {
-    return(list())
-  }
-  return(out)
 }
