fa5c6a87846ada04e27d63370a2e7c908aebacc3
RentoSaijo <rentosaijo0527@gmail.com>
2025-11-22 15:09:59 -0500

    Add new manager functions

---
+++ R/Manager.R
@@ -0,0 +1,21 @@
+#' Get all the general managers
+#' 
+#' `ns_general_managers()` retrieves information on each general manager ...
+#' 
+#' @return data.frame with one row per general manager
+#' @examples
+#' all_GMs <- ns_general_managers()
+#' @export
+
+ns_general_managers <- function() {
+  nhl_api(
+    path = 'general-manager',
+    type = 'r'
+  )$data
+}
+
+#' @rdname ns_general_managers
+#' @export
+ns_gms <- function() {
+  ns_general_managers()
+}
