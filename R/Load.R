8ab8a3e65c0fbf420de54f3eee1b85799574d251
RentoSaijo <rentosaijo0527@gmail.com>
2026-01-27 19:16:25 -0500

    Update load functions

--- R/Load.R
+++ R/Load.R
@@ -13,8 +13,8 @@ gc_play_by_plays <- function(season = 20242025) {
   tryCatch(
     expr = {
       pbps <- utils::read.csv(paste0(
-        'https://media.githubusercontent.com/media/RentoSaijo/NHL_DB/refs/',
-        'heads/main/data/game/pbps/gc/NHL_PBPS_GC_',
+        'https://huggingface.co/datasets/RentoSaijo/NHL_DB/resolve/main/',
+        'data/game/pbps/gc/NHL_PBPS_GC_',
         season,
         '.csv'
       ))
@@ -59,8 +59,8 @@ wsc_play_by_plays <- function(season = 20242025) {
   tryCatch(
     expr = {
       pbps <- utils::read.csv(paste0(
-        'https://media.githubusercontent.com/media/RentoSaijo/NHL_DB/refs/',
-        'heads/main/data/game/pbps/wsc/NHL_PBPS_WSC_',
+        'https://huggingface.co/datasets/RentoSaijo/NHL_DB/resolve/main/',
+        'data/game/pbps/wsc/NHL_PBPS_WSC_',
         season,
         '.csv'
       ))
@@ -88,4 +88,32 @@ wsc_play_by_plays <- function(season = 20242025) {
 #' @export
 wsc_pbps <- function(season = 20242025) {
   wsc_play_by_plays(season)
+}
+
+#' Access the shift charts for a season
+#' 
+#' `shift_charts()` loads the shift charts for a given `season`.
+#' 
+#' @inheritParams roster
+#' @returns data.frame with one row per event (play) per game
+#' @examples
+#' # May take >5s, so skip.
+#' \donttest{shift_charts_20212022 <- shift_charts(season = 20212022)}
+#' @export
+
+shift_charts <- function(season = 20242025) {
+  tryCatch(
+    expr = {
+      utils::read.csv(paste0(
+        'https://huggingface.co/datasets/RentoSaijo/NHL_DB/resolve/main/',
+        'data/game/scs/NHL_SCS_',
+        season,
+        '.csv'
+      ))
+    },
+    error = function(e) {
+      message('Invalid argument(s); refer to help file.')
+      data.frame()
+    }
+  )
 }
